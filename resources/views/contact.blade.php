041538e11d1d74b2f1d1aea47b027a809a4b04cc
Student_PC 3 <gabrielbumaya1@gmail.com>
2024-02-22 18:50:14 +0800

    Initial Commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,1 @@
+<h1>hello contact</h1>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/service', function () {
+    return view('service');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
